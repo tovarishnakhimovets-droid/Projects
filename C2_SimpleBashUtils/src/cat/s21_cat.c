17d5e14cf985fd4a52e385f642539ad39a6b6d64
Hisakobl <tovarishnakhimovets@gmail.com>
2026-08-17 11:00:19 +0300

    argc, argv

--- C2_SimpleBashUtils/src/cat/s21_cat.c
+++ C2_SimpleBashUtils/src/cat/s21_cat.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    printf("argc = %d\n", argc);
+    for (int i = 0; i < argc - 1; i++) {
+        int j = 0;
+        while (argv[i][j] != '\0') {
+            printf("%c", argv[i][j]);
+            j++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
